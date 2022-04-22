df7f40ef1c0f5e04e2b3daa231d1c92db0451e78
Zade <zmaxfield@churchofjesuschrist.org>
2022-04-22 11:21:51 -0600

    UAT updates

--- src/test/java/LDSToolsAppiumTest/MinisteringScreenTest.java
+++ src/test/java/LDSToolsAppiumTest/MinisteringScreenTest.java
@@ -84,7 +84,7 @@ public class MinisteringScreenTest extends BaseDriver {
 
 
 
-    @Test (groups = {"all2", "all", "smoke", "smoke2", "daily", "daily1", "jft"})
+    @Test (groups = {"all2", "all", "smoke", "smoke2", "daily", "daily1"})
     public void ministeringBasic_BISHOP() throws Exception {
         ministeringBasicSub("BISHOP");
     }
@@ -681,7 +681,7 @@ public class MinisteringScreenTest extends BaseDriver {
 
 
 
-    @Test (groups = {"all3", "all", "smoke", "smoke3", "daily", "daily4"})
+    @Test (groups = {"all3", "all", "smoke", "smoke3", "daily", "daily4", "jft"})
     public void companionshipsElders_BISHOP() throws Exception {
         companionshipsEldersSub("BISHOP");
     }
@@ -805,7 +805,7 @@ public class MinisteringScreenTest extends BaseDriver {
             myMinistering.selectDistrict("EQ President");
             Thread.sleep(2000);
             pageSource = myBasePage.getSourceOfPage();
-            Assert.assertTrue(myBasePage.checkNoCaseList("Bryson", pageSource, "Contains"));
+            Assert.assertTrue(myBasePage.checkNoCaseList("Martinez", pageSource, "Contains"));
             Assert.assertFalse(myBasePage.checkNoCaseList("Darth", pageSource, "Contains"));
 
             myBasePage.backButton.click();
