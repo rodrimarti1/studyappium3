d15edc005b55f7ea0fc9cb52bf2490ee29efb324
Zade <zmaxfield@churchofjesuschrist.org>
2022-05-19 13:28:56 -0600

    UAT update

--- src/test/java/LDSToolsAppiumTest/ReportsScreenTest.java
+++ src/test/java/LDSToolsAppiumTest/ReportsScreenTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class ReportsScreenTest extends BaseDriver {
 
 
-    @Test (groups = {"all1", "all", "smoke", "smoke1", "report", "daily", "daily1", "jft"})
+    @Test (groups = {"all1", "all", "smoke", "smoke1", "report", "daily", "daily1"})
     public void reportsBasic_BISHOP() throws Exception {
         reportsBasicCheckSub("BISHOP");
     }
@@ -455,7 +455,7 @@ public class ReportsScreenTest extends BaseDriver {
 
 
     //There is no API for this
-    @Test (groups = {"all3", "all", "report", "daily", "daily4"})
+    @Test (groups = {"all3", "all", "report", "daily", "daily4", "jft"})
     public void reportsYouthRecommendStatus() throws Exception {
         String pageSource;
 
@@ -490,7 +490,7 @@ public class ReportsScreenTest extends BaseDriver {
         pageSource = myBasePage.getSourceOfPage();
 
         //TEST
-        Assert.assertTrue(myBasePage.checkNoCaseList("Conteh", pageSource, "Contains"));
+        Assert.assertTrue(myBasePage.checkNoCaseList("Lucas", pageSource, "Contains"));
         Assert.assertTrue(myBasePage.checkNoCaseList("Proxy", pageSource, "Contains"));
 
         //TODO: Need a way to test this for iOS. iOS does this very different.
