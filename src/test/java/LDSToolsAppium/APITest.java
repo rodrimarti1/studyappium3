ac78250df29d38b1eaf0b50157ded38a439f3819
Zade <zade.maxfield@ldschurch.org>
2022-06-27 15:10:30 -0600

    update usernames

--- src/test/java/LDSToolsAppium/APITest.java
+++ src/test/java/LDSToolsAppium/APITest.java
@@ -172,7 +172,7 @@ public class APITest {
 
 
         for (String callingId: unitMap.keySet()) {
-            memberList = apiTest.getAccounts("39373", callingId);
+            memberList = apiTest.getAccounts("21628", callingId);
             for (String myUsername: memberList) {
 //                System.out.println("Calling Username: " + myUsername);
                 callingAndLoginName.add(callingId + "," + unitMap.get(callingId)  + "," + myUsername );
