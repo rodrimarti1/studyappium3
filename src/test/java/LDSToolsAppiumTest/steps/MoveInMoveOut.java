e98639dda4cee97ff28733226427b3afa6b31d06
Zade <zade.maxfield@ldschurch.org>
2022-12-19 09:24:59 -0700

    Timing change for move in move out

--- src/test/java/LDSToolsAppiumTest/steps/MoveInMoveOut.java
+++ src/test/java/LDSToolsAppiumTest/steps/MoveInMoveOut.java
@@ -71,6 +71,7 @@ public class MoveInMoveOut extends BaseDriver {
 
         //iOS has a select household member's page for one member - this may go away or Android might use this.
         if (myBasePage.getOS().equalsIgnoreCase("ios")) {
+            Thread.sleep(3000);
             Assert.assertTrue(myBasePage.checkForElement(myReports.moveRecordsNext));
             Thread.sleep(1000);
             myBasePage.waitForElementThenClick(myReports.moveRecordsNext);
