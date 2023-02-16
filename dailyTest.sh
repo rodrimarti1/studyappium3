8f91072aa71ecb1ac04a31d599cba66322848ada
Zade <zade.maxfield@ldschurch.org>
2023-02-16 13:42:28 -0700

    Updates to daily test

--- dailyTest.sh
+++ dailyTest.sh
@@ -10,4 +10,4 @@ cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports
 /opt/homebrew/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/AndroidDailyOneDevice.xml
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/target/surefire-reports/testng-results.xml /Users/zmaxfield/Documents/AndroidDaily.xml
 /opt/homebrew/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/AndroidDailyOneCucumber.xml
-cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports1/CucumberTestReport.json /Users/zmaxfield/Documents/AndroidDailyCucumber.json
+cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports/CucumberTestReport.json /Users/zmaxfield/Documents/AndroidDailyCucumber.json
