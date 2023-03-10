590c9c0f47cb302184d7a0e6dd76a0a72a343ee8
Zade <zade.maxfield@ldschurch.org>
2023-03-10 12:53:08 -0700

    remove debug

--- src/main/java/LDSToolsAppium/AppiumService.java
+++ src/main/java/LDSToolsAppium/AppiumService.java
@@ -20,12 +20,12 @@ public class AppiumService {
 
 		String processorType = System.getProperty("os.arch");
 		Map<String, String> map = System.getenv();
-		for (Map.Entry <String, String> entry: map.entrySet()) {
-			System.out.println("Variable Name:- " + entry.getKey() + " Value:- " + entry.getValue());
-			if (entry.getKey().equals("ZAPPIUM_HOME")) {
-				appiumPath = entry.getValue();
-			}
-		}
+//		for (Map.Entry <String, String> entry: map.entrySet()) {
+//			System.out.println("Variable Name:- " + entry.getKey() + " Value:- " + entry.getValue());
+//			if (entry.getKey().equals("ZAPPIUM_HOME")) {
+//				appiumPath = entry.getValue();
+//			}
+//		}
 
 //		System.out.println("Processor: " + processorType);
 //		System.getProperties().list(System.out);
