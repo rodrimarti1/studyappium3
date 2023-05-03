ee32e761b022e73b686639382cb4a131f06a38e3
Zade <zade.maxfield@ldschurch.org>
2023-05-03 15:14:58 -0600

    Fixed Android smoke test

--- smokeTestAndroid.sh
+++ smokeTestAndroid.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /Users/zmaxfield/Documents/workspace/qa-membertools-all
+/opt/homebrew/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/AndroidSmokeOneTests.xml
+sleep 30
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/target/surefire-reports/testng-results.xml /Users/zmaxfield/Documents/AndroidSmoke.xml
 /opt/homebrew/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/AndroidSmokeOneCucumber.xml
 sleep 30
